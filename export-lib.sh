20a741b55d9c01d47229b2052588f7023df3b841
shenxj <2200319548@qq.com>
2018-05-18 05:56:40 -0700

    update

--- export-lib.sh
+++ export-lib.sh
@@ -1,4 +1,5 @@
 objplay_path=~+/
 objplay_lib_path=$objplay_path/output/lib
-export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$objplay_lib_path"
-bash
+string="export LD_LIBRARY_PATH=""$LD_LIBRARY_PATH:$objplay_lib_path"
+
+echo $string
